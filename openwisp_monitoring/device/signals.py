9fe80f1ceb0821b1f882bda82728c107fc2eaf03
akshaynexapp <108393120+akshaynexapp@users.noreply.github.com>
2026-05-19 17:58:51 +0530

    refactor: comment out signal connection for device metrics

--- openwisp_monitoring/device/signals.py
+++ openwisp_monitoring/device/signals.py
@@ -11,10 +11,10 @@ Providing arguments: ['instance', 'request', 'time', 'current']
 """
 
 
-from .notifications.sim_state import handle_sim_state
-from openwisp_monitoring.device.signals import device_metrics_received
-
-device_metrics_received.connect(
-    handle_sim_state,
-    dispatch_uid="sim_state_notification"
-)
+# from .notifications.sim_state import handle_sim_state
+# from openwisp_monitoring.device.signals import device_metrics_received
+#
+# device_metrics_received.connect(
+#     handle_sim_state,
+#     dispatch_uid="sim_state_notification"
+# )
